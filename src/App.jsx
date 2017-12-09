c8cdf7835c978ef37195fef4dfca07ffd69d480b
Jarron Shih <jarronshih@gmail.com>
2017-12-09 10:40:17 -0800

    use outside state

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,31 @@ import Signin from './Signin';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
   render() {
     const testHandleClick = (bid) => {
       console.log(bid);
     };
 
+    const updateLogin = (isLoggedIn) => {
+      this.setState({ isLoggedIn });
+    };
+
     const testProp = {
       handleClick: testHandleClick,
     };
+
+    const authProp = {
+      isLoggedIn: this.state.isLoggedIn,
+      updateLogin,
+    };
+
 
     return (
       <div className="App">
@@ -21,7 +38,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <hr />
-        <Signin />
+        <Signin {...authProp} />
         <hr />
         <RoomInterface {...testProp} />
       </div>
--- src/Signin.jsx
+++ src/Signin.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { authenticateUser } from './cognito';
+
+
+const propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  updateLogin: PropTypes.func.isRequired,
+};
+
 
 class Signin extends Component {
   constructor(props) {
@@ -11,7 +19,6 @@ class Signin extends Component {
     this.state = {
       username: '',
       password: '',
-      isLoggedIn: false,
     };
   }
 
@@ -29,10 +36,12 @@ class Signin extends Component {
     authenticateUser(this.state.username, this.state.password)
       .then((result) => {
         console.log(result);
-        this.setState({ isLoggedIn: true, username: result.accessToken.payload.username });
+        this.setState({ username: result.accessToken.payload.username });
+        this.props.updateLogin(true);
       })
       .catch((err) => {
         console.log(err);
+        this.props.updateLogin(false);
       });
   }
 
@@ -75,11 +84,12 @@ class Signin extends Component {
   }
 
   render() {
-    if (this.state.isLoggedIn) {
+    if (this.props.isLoggedIn) {
       return this.signout();
     }
     return this.signin();
   }
 }
 
+Signin.propTypes = propTypes;
 export default Signin;
