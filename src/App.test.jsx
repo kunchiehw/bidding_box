160fdae5e772199b5454e4af6d370afc6721769f
Jarron Shih <jarronshih@gmail.com>
2017-12-11 16:34:53 -0800

    check welcome message

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,3 +5,11 @@ import App from './App';
 it('renders without crashing', () => {
   shallow(<App />);
 });
+
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1 className="App-title">Welcome to React</h1>;
+  // expect(wrapper.contains(welcome)).to.equal(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
