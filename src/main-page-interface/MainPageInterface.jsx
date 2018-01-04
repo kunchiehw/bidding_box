d59d49450440b120b2fd619600d4209dad50acf5
Kun-Chieh <kunchiehw@gmail.com>
2018-01-04 14:47:03 -0800

    Add logo to temporary main page

--- src/main-page-interface/MainPageInterface.jsx
+++ src/main-page-interface/MainPageInterface.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
+import './MainPageInterface.css';
+import logo from '../logo.svg';
 
-function MainPageInterface() {
+function MainPageInterface(props) {
+  const handleToLoginButtonClick = () => {
+    props.history.push('/login');
+  };
+
   return (
     <div>
-      <div>
-        Lovely (?) Side Project
-      </div>
-      <Link to="/login">
-        <Button className="to-login-button" size="small">
-          Log in
-        </Button>
-      </Link>
+      <header className="main-page-header">
+        <img src={logo} className="main-page-logo" alt="logo" />
+        <h1 className="main-page-title">Welcome to React</h1>
+      </header>
+      <hr />
+      <Button className="to-login-button" size="small" onClick={handleToLoginButtonClick}>
+        Log in
+      </Button>
     </div>
   );
 }
 
-export default MainPageInterface;
+export default withRouter(MainPageInterface);
