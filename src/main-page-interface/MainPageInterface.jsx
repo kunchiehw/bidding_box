f359f1ff9f72a85e067abe295c8bf4a36eb78f89
Kun-Chieh <kunchiehw@gmail.com>
2018-01-06 14:50:33 -0800

    Add propTypes in MainPageInterface

--- src/main-page-interface/MainPageInterface.jsx
+++ src/main-page-interface/MainPageInterface.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 import './MainPageInterface.css';
 import logo from '../logo.svg';
+
+const propTypes = {
+  // props from HOC withRouter
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 function MainPageInterface(props) {
   const handleToLoginButtonClick = () => {
@@ -10,17 +18,18 @@ function MainPageInterface(props) {
   };
 
   return (
-    <div>
+    <div className="main-page-interface">
       <header className="main-page-header">
         <img src={logo} className="main-page-logo" alt="logo" />
         <h1 className="main-page-title">Welcome to React</h1>
       </header>
-      <hr />
-      <Button className="to-login-button" size="small" onClick={handleToLoginButtonClick}>
-        Log in
+      <Button className="to-login-button" size="small" onClick={handleToLoginButtonClick} color="green">
+        <Icon name="sign in" />Log in
       </Button>
     </div>
   );
 }
+
+MainPageInterface.propTypes = propTypes;
 
 export default withRouter(MainPageInterface);
