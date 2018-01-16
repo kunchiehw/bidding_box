9db25053e64285aade2c31f42498f0ae399accc6
Kun-Chieh <kunchiehw@gmail.com>
2018-01-15 23:12:27 -0800

    update unit test in BidButtonBlock

--- src/room-interface/bid-button-block/BidButtonBlock.test.jsx
+++ src/room-interface/bid-button-block/BidButtonBlock.test.jsx
@@ -1,96 +1,109 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SuitBidButton, { suitBidButtonDisabled } from './SuitBidButton';
-import OtherBidButton from './OtherBidButton';
+import toJson from 'enzyme-to-json';
+import BidButton from './BidButton';
 import BidButtonBlock from './BidButtonBlock';
+import { shouldStandardBidButtonDisabled } from './helper-BidButtonBlock';
 
-it('SuitBidButton render', () => {
-  const prop1 = {
-    bid: {
-      level: 3,
-      suit: 'NOTRUMPS',
-    },
-    curBid: null,
-    handleClick: () => null,
-    isDisabled: false,
-  };
-
-  shallow(<SuitBidButton {...prop1} />);
-
-  const prop2 = {
-    bid: {
-      level: 2,
-      suit: 'HEARTS',
-    },
-    curBid: {
-      level: 4,
-      suit: 'SPADES',
-    },
-    handleClick: bid => bid,
-    isDisabled: true,
-  };
-
-  shallow(<SuitBidButton {...prop2} />);
+it('helper shouldStandardBidButtonDisabled', () => {
+  expect(shouldStandardBidButtonDisabled(0, 'PASS', 1, 'CLUBS')).toEqual(false);
+  expect(shouldStandardBidButtonDisabled(2, 'SPADES', 2, 'NOTRUMPS')).toEqual(false);
+  expect(shouldStandardBidButtonDisabled(2, 'NOTRUMPS', 3, 'SPADES')).toEqual(false);
+  expect(shouldStandardBidButtonDisabled(4, 'HEARTS', 4, 'DIAMONDS')).toEqual(true);
+  expect(shouldStandardBidButtonDisabled(5, 'DIAMONDS', 4, 'HEARTS')).toEqual(true);
 });
 
-it('SuitBidButton function suitBidButtonDisabled', () => {
-  expect(suitBidButtonDisabled(null, { level: 1, suit: 'CLUBS' })).toEqual(false);
-  expect(suitBidButtonDisabled({ level: 2, suit: 'SPADES' }, { level: 2, suit: 'NOTRUMPS' })).toEqual(false);
-  expect(suitBidButtonDisabled({ level: 2, suit: 'NOTRUMPS' }, { level: 3, suit: 'SPADES' })).toEqual(false);
-});
+it('BidButton render', () => {
+  const bidButton1 = shallow(<BidButton
+    level={0}
+    suit="PASS"
+    isDisabled={false}
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton1)).toMatchSnapshot();
 
-it('OtherBidButton render', () => {
-  const prop1 = {
-    suit: 'PASS',
-    handleClick: () => null,
-    isDisabled: false,
-  };
+  const bidButton2 = shallow(<BidButton
+    level={0}
+    suit="DOUBLE"
+    isDisabled
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton2)).toMatchSnapshot();
 
-  shallow(<OtherBidButton {...prop1} />);
+  const bidButton3 = shallow(<BidButton
+    level={0}
+    suit="REDOUBLE"
+    isDisabled={false}
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton3)).toMatchSnapshot();
 
-  const prop2 = {
-    suit: 'REDOUBLE',
-    handleClick: bid => bid,
-    isDisabled: true,
-  };
+  const bidButton4 = shallow(<BidButton
+    level={1}
+    suit="CLUBS"
+    isDisabled
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton4)).toMatchSnapshot();
 
-  shallow(<OtherBidButton {...prop2} />);
+  const bidButton5 = shallow(<BidButton
+    level={2}
+    suit="DIAMONDS"
+    isDisabled={false}
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton5)).toMatchSnapshot();
+
+  const bidButton6 = shallow(<BidButton
+    level={3}
+    suit="HEARTS"
+    isDisabled
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton6)).toMatchSnapshot();
+
+  const bidButton7 = shallow(<BidButton
+    level={5}
+    suit="SPADES"
+    isDisabled={false}
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton7)).toMatchSnapshot();
+
+  const bidButton8 = shallow(<BidButton
+    level={7}
+    suit="NOTRUMPS"
+    isDisabled
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButton8)).toMatchSnapshot();
 });
 
 it('BidButtonBlock render', () => {
-  const prop1 = {
-    curBid: null,
-    disabledDouble: true,
-    disabledRedouble: true,
-    handleClick: () => null,
-    isDisabled: false,
-  };
+  const bidButtonBlock1 = shallow(<BidButtonBlock
+    currentLevel={0}
+    currentSuit="PASS"
+    shouldDoubleButtonDisabled
+    shouldRedoubleButtonDisabled
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButtonBlock1)).toMatchSnapshot();
 
-  shallow(<BidButtonBlock {...prop1} />);
+  const bidButtonBlock2 = shallow(<BidButtonBlock
+    currentLevel={3}
+    currentSuit="NOTRUMPS"
+    shouldDoubleButtonDisabled={false}
+    shouldRedoubleButtonDisabled
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButtonBlock2)).toMatchSnapshot();
 
-  const prop2 = {
-    curBid: {
-      level: 1,
-      suit: 'NOTRUMPS',
-    },
-    disabledDouble: true,
-    disabledRedouble: false,
-    handleClick: bid => bid,
-    isDisabled: false,
-  };
-
-  shallow(<BidButtonBlock {...prop2} />);
-
-  const prop3 = {
-    curBid: {
-      level: 7,
-      suit: 'SPADES',
-    },
-    disabledDouble: false,
-    disabledRedouble: true,
-    handleClick: () => null,
-    isDisabled: true,
-  };
-
-  shallow(<BidButtonBlock {...prop3} />);
+  const bidButtonBlock3 = shallow(<BidButtonBlock
+    currentLevel={7}
+    currentSuit="HEARTS"
+    shouldDoubleButtonDisabled
+    shouldRedoubleButtonDisabled={false}
+    handleBidButtonClick={() => null}
+  />);
+  expect(toJson(bidButtonBlock3)).toMatchSnapshot();
 });
