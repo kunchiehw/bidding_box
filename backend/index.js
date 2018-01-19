fb930b49045a2631dcac299ced371fcfc52b633d
Jarron Shih <jarronshih@gmail.com>
2018-01-18 19:41:37 -0800

    set bodyParser

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
+app.use(bodyParser.json());
 
 server.listen(8080, () => {
   console.log('Listening on %d', server.address().port);
@@ -35,7 +36,6 @@ wss.on('connection', broadcastWs.onConnect);
 // API define
 app.post(
   '/token',
-  bodyParser.json(),
   (req, res) => {
     if (!req.body) {
       return res.sendStatus(400);
