8bb013853360098c47f71c28cbbb0ca5b449512e
Jarron Shih <jarronshih@gmail.com>
2018-01-23 23:32:05 -0800

    ws testcase

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "env": {
     "browser": true,
     "jest": true,
+    "mocha": true,
   },
   "extends": "airbnb",
   "rules": {
--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -1,30 +1,73 @@
+require('dotenv').config();
 const request = require('supertest');
 const { server } = require('../index');
+const WebSocket = require('ws');
 
 const auth = { username: 'unittest', password: 'password' };
 
 
 describe('loading express', () => {
-  after((done) => {
-    server.close(done);
+  // after((done) => {
+  //   server.close(done);
+  // });
+
+  describe('RESTful', () => {
+    it('GET /', (done) => {
+      request(server)
+        .get('/')
+        .expect(200, done);
+    });
+
+    it('404 everything else', (done) => {
+      request(server)
+        .get('/foo/bar')
+        .expect(404, done);
+    });
+
+    it('POST /token', (done) => {
+      request(server)
+        .post('/token')
+        .send(auth)
+        .expect(200, done);
+    });
+
+    it('POST /token', (done) => {
+      request(server)
+        .post('/token')
+        .send(auth)
+        .expect(200, done);
+    });
   });
 
-  it('GET /', (done) => {
-    request(server)
-      .get('/')
-      .expect(200, done);
-  });
+  describe('websocket', () => {
+    let token = null;
+    const wsUrl = `ws://localhost:${process.env.PORT || 8080}`;
 
-  it('404 everything else', (done) => {
-    request(server)
-      .get('/foo/bar')
-      .expect(404, done);
-  });
+    before((done) => {
+      request(server)
+        .post('/token')
+        .send(auth)
+        .then((res) => {
+          token = res.text;
+          request(server)
+            .post('/room/test')
+            .set('Authorization', `Bearer ${token}`)
+            .then(() => {
+              done();
+            });
+        });
+    });
 
-  it('POST /token', (done) => {
-    request(server)
-      .post('/token')
-      .send(auth)
-      .expect(200, done);
+    it('/room/:roomId', (done) => {
+      const ws = new WebSocket(`${wsUrl}/room/test`);
+
+      ws.on('open', () => {
+        ws.send(token);
+      });
+
+      ws.on('message', (data) => {
+        done();
+      });
+    });
   });
 });
